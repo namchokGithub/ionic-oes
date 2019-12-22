d9f31340d6db6c8d0e2e513a0e72c89c2e8bb686
namchokGithub <pech4751@gmail.com>
2019-12-22 14:20:32 +0700

    list test page

--- src/app/online_testing/list-testing/list-testing.page.ts
+++ src/app/online_testing/list-testing/list-testing.page.ts
@@ -1,56 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { OnlineTestingService } from 'src/app/api/online-testing.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { OnlineTestingService } from "src/app/api/online-testing.service";
+import { Router } from "@angular/router";
 @Component({
-  selector: 'app-list-testing',
-  templateUrl: './list-testing.page.html',
-  styleUrls: ['./list-testing.page.scss'],
+  selector: "app-list-testing",
+  templateUrl: "./list-testing.page.html",
+  styleUrls: ["./list-testing.page.scss"]
 })
 export class ListTestingPage implements OnInit {
+  public listtest: any[];
+  public subject: any[];
+  private name_subject: string;
+  private type_exam: number;
+  private type_exam_name: string;
+  private hide = false;
 
-  public listtest: any[]
-  public subject: any[]
-  private name_subject: string
-  private type_exam: number
-
-  constructor(public OnlineTestingService: OnlineTestingService, public router: Router) {
-    this.get_testing()
-    this.get_subject()
+  constructor(
+    public OnlineTestingService: OnlineTestingService,
+    public router: Router
+  ) {
+    this.get_testing();
+    this.get_subject();
   }
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   get_subject() {
-    this.OnlineTestingService.get_subject().subscribe((response) => {
-      this.subject = response
-      console.log(response)
+    this.OnlineTestingService.get_subject().subscribe(response => {
+      this.subject = response;
     });
   }
 
   get_testing() {
-    this.OnlineTestingService.getTesting().subscribe((response) => {
+    this.OnlineTestingService.getTesting().subscribe(response => {
       this.listtest = response;
-      console.log(response)
     });
   }
-  
+
   onGoToNextPage(id: number) {
-    this.router.navigate(['/detail-testing'], {
+    this.router.navigate(["/detail-testing"], {
       queryParams: {
         asg_id: id
       }
     });
   }
 
-  con() {
-    console.log(this.name_subject)
+  check() {
+    // console.log(this.name_subject)
+    // console.log(this.type_exam)
+    if (this.type_exam!=null && this.name_subject!=null) {
+      this.hide = true;
+      if (this.type_exam == 1) this.type_exam_name = "(เก็บคะแนน)";
+      else if (this.type_exam == 2) this.type_exam_name = "(กลางภาค)";
+      else if (this.type_exam == 3) this.type_exam_name = "(ปลายภาค)";
+    } else {
+      this.hide = false;
+    }
   }
-
-  test(){
-    console.log(this.name_subject)
-    console.log(this.type_exam)
-  }
-
 }
