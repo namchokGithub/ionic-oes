3def916b0324fb9eecaf2720d3f58c52a470a67a
namchokGithub <pech4751@gmail.com>
2019-12-22 13:32:39 +0700

    review student

--- src/app/Exam_review/review-student/review-student.page.ts
+++ src/app/Exam_review/review-student/review-student.page.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import {ReviewTestingService} from 'src/app/api/review-testing.service'
+import { Component, OnInit } from "@angular/core";
+import { ReviewTestingService } from "src/app/api/review-testing.service";
+import { ToastController } from "@ionic/angular";
+import { AlertController } from '@ionic/angular';
+
 @Component({
-  selector: 'app-review-student',
-  templateUrl: './review-student.page.html',
-  styleUrls: ['./review-student.page.scss'],
+  selector: "app-review-student",
+  templateUrl: "./review-student.page.html",
+  styleUrls: ["./review-student.page.scss"]
 })
 export class ReviewStudentPage implements OnInit {
-
   private crsId: number;
   private Year: number;
   private crs_term: number;
@@ -15,32 +17,44 @@ export class ReviewStudentPage implements OnInit {
   private rs_subject: [];
   private rs_result_exam_student: [];
 
-  constructor(private ReviewTestingService:ReviewTestingService) { }
+  private hide = false;
+
+  constructor(
+    private ReviewTestingService: ReviewTestingService,
+    public toastController: ToastController,
+    private alertController: AlertController
+  ) {}
 
   ngOnInit() {
-    this.get_subject()
+    this.get_subject();
     // this.show_result_exam_student()
   }
 
   get_subject() {
-
     this.ReviewTestingService.get_subject().subscribe(result => {
-      console.log(result)
-      this.rs_subject = result.rs_subject
-      this.rs_year = result.rs_year
-      console.log(this.rs_subject)
-      console.log(this.rs_year)
-
-    })
-
+      this.rs_subject = result.rs_subject;
+      this.rs_year = result.rs_year;
+    });
   }
 
-  show_result_exam_student() {
-    console.log('show_result_exam_student')
-    this.ReviewTestingService.show_result_exam_student(this.crsId, this.Year, this.crs_term).subscribe(result => {
-      this.rs_result_exam_student = result
-      console.log(this.rs_result_exam_student)
-    })
-
+  async show_result_exam_student() {
+    if (this.crsId == null) {
+      const alert = await this.alertController.create({
+        message: 'กรุณาเลือกวิชา',
+        buttons: ['OK']
+      });
+      alert.present();
+      console.log("NULL");
+    } else {
+      this.ReviewTestingService.show_result_exam_student(
+        this.crsId,
+        this.Year,
+        this.crs_term
+      ).subscribe(result => {
+        this.rs_result_exam_student = result;
+        this.hide = true;
+        // console.log(this.rs_result_exam_student)
+      });
+    }
   }
 }
