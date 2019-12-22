b02fcddd9faf5340a4585d3cf24c83eaf77e24c9
namchokGithub <pech4751@gmail.com>
2019-12-22 11:12:31 +0700

    modified home login set toast

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
 import { MenuController, LoadingController, NavController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 
 @Component({
@@ -10,16 +11,34 @@ import { MenuController, LoadingController, NavController } from '@ionic/angular
 })
 export class HomePage {
 
-  private username : string;
+  private username : string = "";
 
-  constructor(private menuCtrl: MenuController, private loadingController: LoadingController, private navCtrl: NavController) { }
+  constructor(private menuCtrl: MenuController, private loadingController: LoadingController, private navCtrl: NavController,public toastController: ToastController) { }
   
   ngOnInit() {
     this.menuCtrl.swipeEnable(false);
   }
 
+  validate(){
+    if(this.username!=""){
+      this.login()
+    }else{
+      this.presentToast()
+    }
+  }
+
+  async presentToast() {
+    const toast =  await this.toastController.create({
+      message: 'กรุณาใส่ชื่อผู้ใช้',
+      duration: 3000,
+      position: 'bottom',
+      showCloseButton: true
+    });
+    toast.present();
+  }
+
   async login() {
-    console.log(this.username)
+    // console.log(this.username)
     const loading = await this.loadingController.create({
       spinner: 'lines',
       duration: 100,
