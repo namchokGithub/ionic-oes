abe12be4b85c0a7bc5fd8c89f2d7200e992c5c9b
namchokGithub <pech4751@gmail.com>
2020-01-12 03:58:58 +0700

    4.00 am

--- src/app/regis/regis/regis.page.ts
+++ src/app/regis/regis/regis.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NavController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-regis',
-  templateUrl: './regis.page.html',
-  styleUrls: ['./regis.page.scss'],
+  selector: "app-regis",
+  templateUrl: "./regis.page.html",
+  styleUrls: ["./regis.page.scss"]
 })
 export class RegisPage implements OnInit {
+  constructor(private navCtrl: NavController) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  back() {
+    this.navCtrl.navigateRoot(["/home"]);
   }
 
+  // Send username
+  regis(){
+    this.navCtrl.navigateRoot(["/home"]);
+  }
 }
