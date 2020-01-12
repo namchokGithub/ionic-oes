796cd1eb8dc0c6c1ed305781b377230325214a10
namchokGithub <pech4751@gmail.com>
2020-01-12 22:34:23 +0700

    edit item subject

--- src/app/admin/admin/admin.page.ts
+++ src/app/admin/admin/admin.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPage implements OnInit {
 
-  constructor() { }
+  private crsId: number;
+  private Year: number;
+  private crs_term: number;
+  
+  private rs_subject: []
+
+  private hide = false;
+
+  constructor(public alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  // Add subject item
+  async addItem() {
+
+    const alert = await this.alertController.create({
+      header: "ต้องการสร้างชุดข้อสอบ",
+      // message: "ยืนยันการสมัคร",
+      buttons: [
+        {
+          text: "ยกเลิก",
+          role: "cancel",
+          cssClass: "secondary",
+          handler: () => {
+            console.log("Confirm Cancel");
+          }
+        },
+        {
+          text: "ยืนยัน",
+          role: "submit",
+          cssClass: "secondary",
+          handler: () => {
+            
+          }
+        }
+      ]
+    });
+    await alert.present();
   }
 
 }
