bd734dcc161730b888b9a64177438efc661bc198
namchokGithub <pech4751@gmail.com>
2020-01-13 16:38:18 +0700

    update all

--- src/app/admin/admin/admin.page.ts
+++ src/app/admin/admin/admin.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController, NavController } from '@ionic/angular';
+import { ExamServiceService } from "src/app/api/exam-service.service";
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -9,21 +11,25 @@ import { AlertController, NavController } from '@ionic/angular';
 export class AdminPage implements OnInit {
 
   private crsId: number;
-  private Year: number;
-  private crs_term: number;
-  
   private rs_subject: []
+  private rs_exam: []
+ 
 
-  private hide = false;
-
-  constructor(private navCtrl: NavController, public alertController: AlertController) { }
+  constructor(
+    private ExamServiceService: ExamServiceService,
+    private navCtrl: NavController, 
+    public alertController: AlertController,
+    public activatedRoute: ActivatedRoute, 
+    public router: Router
+    ) { }
 
   ngOnInit() {
+    this.get_subject_all()
+    
   }
 
   // Add subject item
   async addItem() {
-
     const alert = await this.alertController.create({
       header: "ต้องการสร้างชุดข้อสอบ",
       // message: "ยืนยันการสมัคร",
@@ -49,4 +55,67 @@ export class AdminPage implements OnInit {
     await alert.present();
   }
 
+  async delete_exam(ib_id:number) {
+    const alert = await this.alertController.create({
+      header: "ต้องการลบชุดข้อสอบ",
+      // message: "ยืนยันการสมัคร",
+      buttons: [
+        {
+          text: "ยกเลิก",
+          role: "cancel",
+          cssClass: "secondary",
+          handler: () => {
+            console.log("Confirm Cancel");
+          }
+        },
+        {
+          text: "ยืนยัน",
+          role: "submit",
+          cssClass: "secondary",
+          handler: () => {
+            this.ExamServiceService.delete_pers_item_bank(ib_id).subscribe((res) => {
+              this.get_exam_all()
+            });
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  get_subject_all() {
+    this.ExamServiceService.get_subject_all().subscribe((res) => {
+      console.log(res)
+      this.rs_subject = res
+    });
+  }
+
+  get_exam_all(){
+    this.ExamServiceService.get_pers_item_bank(this.crsId).subscribe((res) => {
+      console.log(res)
+      this.rs_exam = res
+    });
+  }
+
+  searchSubject(){
+    console.log(this.crsId);
+    this.get_exam_all()
+  }
+
+  edit(ib_id:number){
+    console.log(ib_id)
+    this.onGoeditpage(ib_id)
+  }
+
+  delete(ib_id:number){
+    console.log(ib_id)
+  }
+
+  onGoeditpage(ib_id:number) {
+    this.router.navigate(['/edit'], {
+      queryParams: {
+        ib_id: ib_id
+      }
+    });
+  }
 }
--- src/app/item/item/item.page.ts
+++ src/app/item/item/item.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
+import { ExamServiceService } from "src/app/api/exam-service.service";
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-item',
@@ -6,10 +9,95 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.page.scss'],
 })
 export class ItemPage implements OnInit {
-
-  constructor() { }
+  private ib_code:string
+  private ib_name:string
+  private ib_sub_id:number
+  private ib_stem_amount:number 
+  private ib_option_amount:number
+  private ib_question_insert:[]
+  private ib_question:[]
+  private rs_subject:[]
+  constructor(
+    private examServiceService: ExamServiceService,
+    private navCtrl: NavController, 
+    public alertController: AlertController,
+    public activatedRoute: ActivatedRoute, 
+    public router: Router
+  ) { }
 
   ngOnInit() {
+    this.get_subject_all()
+    this.get_question_all()
   }
 
+  get_subject_all() {
+    this.examServiceService.get_subject_all().subscribe((res) => {
+      console.log(res)
+      this.rs_subject = res
+    });
+  }
+
+  get_question_all(){
+    this.examServiceService.get_pers_question_all().subscribe((res) => {
+      console.log(res)
+      this.ib_question = res
+    });
+  }
+
+  async save_Exam(){
+    const alert = await this.alertController.create({
+      header:  `ยืนยันการสร้างชุดข้อสอบ`,
+      message: ` รหัสชุดข้อสอบ ${this.ib_code} <br>
+                 ชื่อชุดข้อสอบ ${this.ib_name} <br>
+                 จำนวน ${this.ib_stem_amount} ข้อ`,
+      buttons: [
+        {
+          text: "ยกเลิก",
+          role: "cancel",
+          cssClass: "secondary",
+          handler: () => {
+            console.log("Confirm Cancel");
+          }
+        },
+        {
+          text: "ยืนยัน",
+          role: "submit",
+          cssClass: "secondary",
+          handler: () => {
+            console.log(this.ib_question_insert)
+            this.examServiceService.insert_pers_item_bank(this.ib_code,this.ib_name,this.ib_sub_id,this.ib_stem_amount,this.ib_option_amount).subscribe((res) => {
+              this.router.navigate(['/admin'])
+            });
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+ 
+  async cancle_Exam(){
+    const alert = await this.alertController.create({
+      header: "ยกเลิกการแก้ไข",
+      //message: "ยกเลิกการแก้ไข",
+      buttons: [
+        {
+          text: "ยกเลิก",
+          role: "cancel",
+          cssClass: "secondary",
+          handler: () => {
+            console.log("Confirm Cancel");
+          }
+        },
+        {
+          text: "ยืนยัน",
+          role: "submit",
+          cssClass: "secondary",
+          handler: () => {
+              this.router.navigate(['/admin'])          
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 }
